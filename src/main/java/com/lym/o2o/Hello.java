e39d968517c3ee248f2d0bd89973c28f32fe2bfe
Nero_Li <375892799@qq.com>
2019-04-09 12:17:39 +0800

    Initial commit

---
+++ src/main/java/com/lym/o2o/Hello.java
@@ -0,0 +1,20 @@
+package com.lym.o2o;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * test
+ * @ClassName Hello
+ * @Author lyming
+ * @Date 2019/4/9 12:01
+ **/
+@RestController
+public class Hello {
+
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    private String hello(){
+        return "Hello SpringBoot";
+    }
+}
